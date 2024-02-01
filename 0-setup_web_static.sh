9d9d204f13236905b7e750b7f187a9acffe96caf
Ailwei <Ailweimaemu@gmail.com>
2024-02-01 00:05:18 -0800

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
 #script that set the servers for deployment of we_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
-# Create necesasry directions for web_static
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/releases/test/index.html
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
 
-# create the html link 
-sudo echo "<html>
+# Define the folders
+folders=("/data" "/data/web_static" "/data/web_static/releases" "/data/web_static/shared" "/data/web_static/releases/test")
+
+# Create necessary folders if they don't exist
+for folder in "${folders[@]}"
+do
+    if [ ! -d "$folder" ]; then
+        sudo mkdir -p "$folder"
+    fi
+done
+
+# Create a fake HTML file
+echo "<html>
   <head>
   </head>
   <body>
-   Holberton School
+    Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create symbolic link, delete if already exists
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+# Give ownership to the current user and group recursively
+sudo chown -R $USER:$USER /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbtn_static { alias /data/web_static/current/:}' /etc/nginx/sites-enabled/default
+# Update Nginx configuration
+sudo sed -i '#/location /hbnb_static/ { s#alias .*#alias /data/web_static/current/; }#' /etc/nginx/sites-available/default
 
+# Restart Nginx
 sudo service nginx restart
+
+exit 0
