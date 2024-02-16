6a9c990ebb7e20c6f8d441c4cec7d72ed62100d6
Ailwei <Ailweimaemu@gmail.com>
2024-02-16 01:54:02 -0800

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -13,6 +13,8 @@ Routes:
 """
 
 from flask import Flask
+from werkzeug.utils import escape
+
 
 app = Flask(__name__)
 
@@ -28,8 +30,15 @@ def hbnb():
 
 
 @app.route('/c/text>', strict_slashes=False)
-def c_text():
+def c_text(text):
     return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    return 'Python {}'.format(escape(text).replace('_', ' '))
+
 
 
 if __name__ == '__main__':
