c69100534e6e120f3e07fd084312b06cf27df358
Ailwei <Ailweimaemu@gmail.com>
2024-02-16 01:39:46 -0800

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script to start a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ”, followed by the value of the text variable
+                (replace underscore _ symbols with a space )
+    /python/<text>: display “Python ”,
+    followed by the value of the text variable
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/text>', strict_slashes=False)
+def c_text():
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
