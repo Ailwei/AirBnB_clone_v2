deb46c1da46cd098269337314aaaa8d25f8c4787
Ailwei <Ailweimaemu@gmail.com>
2024-02-17 00:47:44 -0800

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,11 +13,17 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(exception):
+    """
+    teardown methods to clear up 
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """
+    display states
+    """
     states = sorted(storage.all(State).values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
